ef71456ee6de05ca52df4033baded66e74d17c59
Ryan-Hozy <ryanho.rhzy@gmail.com>
2024-08-22 07:50:57 +0800

    initial commit. Completed the setup for the first mobile app development

---
+++ app/profile.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Dameh</Text>
+    </View>
+  )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})
